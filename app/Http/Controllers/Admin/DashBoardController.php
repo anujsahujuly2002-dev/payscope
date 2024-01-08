e1ab27c0e1209b1e85ada86d87f27b6dfd3593ab
Anuj Kumar <anujsahujuly2002@gmail.com>
2024-01-08 15:31:54 +0530

    logout functionality

--- app/Http/Controllers/Admin/DashBoardController.php
+++ app/Http/Controllers/Admin/DashBoardController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Wallet;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\LoginSession;
+use Carbon\Carbon;
 
 class DashBoardController extends Controller
 {
@@ -13,6 +15,11 @@ class DashBoardController extends Controller
         return view('admin.dashboard');
     }
     public function logout (Request $request) {
+        $user = auth()->user()->id;
+        LoginSession::where('user_id',$user)->update([
+            'is_logged_in' => '1',
+            'logout_time'=>Carbon::now()->format('Y-m-d H:i:s')
+        ]);
         Auth::logout();    
         $request->session()->invalidate();
         $request->session()->regenerateToken();
