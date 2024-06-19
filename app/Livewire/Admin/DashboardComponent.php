b0215b5f55f68c099a1e7288e35f45a05517f078
Divya Pandey <divya.pandey@groscope.com>
2024-06-19 17:17:30 +0530

    showing login session in recent activity

--- app/Livewire/Admin/DashboardComponent.php
+++ app/Livewire/Admin/DashboardComponent.php
@@ -3,11 +3,12 @@
 namespace App\Livewire\Admin;
 
 use Livewire\Component;
-
+use App\Models\LoginSession;
 class DashboardComponent extends Component
 {
     public function render()
     {
-        return view('livewire.admin.dashboard-component');
+        $data['data'] = LoginSession::with('user')->orderBy('created_at', 'desc')->get();
+        return view('livewire.admin.dashboard-component',$data);
     }
 }
