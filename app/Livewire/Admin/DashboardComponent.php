740731dab3baf1553439da1add375656e61fab23
Anuj Kuamr <anujsahujuly2002@gmail.com>
2024-07-05 04:18:46 +0530

    dashboard component reslove conflict

--- app/Livewire/Admin/DashboardComponent.php
+++ app/Livewire/Admin/DashboardComponent.php
@@ -11,13 +11,6 @@ class DashboardComponent extends Component
 {
     public function render()
     {
-<<<<<<< HEAD
-        $loginActivities = LoginSession::when(auth()->user()->getRoleNames()->first() !='super-admin',function($query){
-            $query->where('user_id',auth()->user()->id);
-        })
-        ->with('user')->latest()->take(10)->get();
-        return view('livewire.admin.dashboard-component',compact('loginActivities'));
-=======
         $data['loginActivities'] = LoginSession::with('user')->orderBy('created_at', 'desc')->take(10)->get();
         $data['payout'] = PayoutRequestHistory::when(auth()->user()->getRoleNames()->first() !='super-admin',function($q){
             $q->where('user_id',auth()->user()->id);
@@ -32,6 +25,5 @@ class DashboardComponent extends Component
             $r->where('user_id',auth()->user()->id);
         })->where('status_id',2)->sum('charge');
         return view('livewire.admin.dashboard-component',$data);
->>>>>>> eca7b91e5730bb2ce7e5e46095a9e47db2c71814
     }
 }
