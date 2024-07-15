78ff167021976c44936f450ede1c5ea7706c805a
Divya Pandey <divya.pandey@groscope.com>
2024-07-15 18:51:14 +0530

    ...

--- resources/views/livewire/admin/auth/otp-verification-component.blade.php
+++ resources/views/livewire/admin/auth/otp-verification-component.blade.php
@@ -3,7 +3,7 @@
         <div class="row d-flex justify-content-end align-items-end">
             <div class="col-md-8">
                 <div class="textstyle p-5">
-                    <h3>Welcome to the Dummy Text Generator!</h3>
+                    <h3>Welcome to Groscope!</h3>
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Cum
                         sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                 </div>
@@ -15,7 +15,7 @@
                         <h1>Forgot Your Password</h1>
                         <h6>Please enter the one time password to verify your account</h6>
                         <div> <span>A code has been sent to</span> <small>@if ($message = Session::get('mobile_no')) {{ $message }}@endif</small> </div>
-                       
+
                         <div id="otp" class="inputs d-flex flex-row justify-content-center mt-2">
                             <input class="m-2 text-center form-control rounded" type="text" id="first" wire:model="otp.0" autocomplete="off">
                             <input class="m-2 text-center form-control rounded" type="text" id="second"  wire:model="otp.1" autocomplete="off">
@@ -34,7 +34,7 @@
     <script>
     document.addEventListener("DOMContentLoaded", function(event) {
         function OTPInput() {
-            const inputs = document.querySelectorAll('#otp > *[id]'); 
+            const inputs = document.querySelectorAll('#otp > *[id]');
             // console.log(inputs);
             for (let i = 0; i < inputs.length; i++) {
                 inputs[i].addEventListener('keydown', function(event) {
