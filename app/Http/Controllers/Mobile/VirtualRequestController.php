985941716223a5bf554c31a80f4d469b4aef73cb
Divya Pandey <divya.pandey@groscope.com>
2024-07-24 17:58:29 +0530

    Api virtual request1

--- app/Http/Controllers/Mobile/VirtualRequestController.php
+++ app/Http/Controllers/Mobile/VirtualRequestController.php
@@ -14,19 +14,20 @@ class VirtualRequestController extends Controller
 
     public function virtualRequest()
     {
-        $virtualRequest = VirtualRequest::where('user_id', auth()->user()->id)->with(['user', 'bank','fund'])->get();
+        // $data = VirtualRequest::where('user_id', auth()->user()->id)->with(['user', 'bank','fund'])->get();
+        $data = VirtualRequest::all();
         $result = [];
-        if ($virtualRequest->count() > 0) :
-            foreach ($virtualRequest as $virtual) {
+        if ($data->count() > 0) :
+            foreach ($data as $virtual) {
                 $result[] = [
                     'id' => $virtual->id,
                     'credit_time' => $virtual->credit_time,
                     'remitter_name' => $virtual->remitter_name,
-                    'order_amount' => $virtual->bank->credit_amount,
-                    'remitter_account_number' => $virtual->bank->remitter_account_number,
-                    'reference_number' => $virtual->bank->reference_number,
-                    'remitter_ifsc_code' => $virtual->bank->remitter_ifsc_code,
-                    'remitter_utr' => $virtual->bank->remitter_utr,
+                    'order_amount' => $virtual->credit_amount,
+                    'remitter_account_number' => $virtual->remitter_account_number,
+                    'reference_number' => $virtual->reference_number,
+                    'remitter_ifsc_code' => $virtual->remitter_ifsc_code,
+                    'remitter_utr' => $virtual->remitter_utr,
                 ];
             }
             endif;
