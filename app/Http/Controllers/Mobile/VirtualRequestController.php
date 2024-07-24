f274605b914e3b843256f9a84d1147543d100907
Divya Pandey <divya.pandey@groscope.com>
2024-07-24 11:19:20 +0530

    create api for virtual fund1

--- app/Http/Controllers/Mobile/VirtualRequestController.php
+++ app/Http/Controllers/Mobile/VirtualRequestController.php
@@ -9,44 +9,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class VirtualRequestController extends Controller
-// {
-
-//     public function virtualRequest()
-//     {
-//         $data = VirtualRequest::where('user_id', auth()->user()->id)->get();
-//         $result = [];
-//         if ($data->count() > 0) :
-//             foreach ($data as $virtual) {
-//                 $result[] = [
-//                     'id' => $virtual->id,
-//                     'user_name' => $virtual->virtualRequest->user->name,
-//                     'bank_name' => $virtual->bank->name,
-//                     'amount' => $virtual->amount,
-//                     'account_number' => $virtual->bank->account_number,
-//                     'branch_name' => $virtual->bank->branch_name,
-//                     'ifsc_code' => $virtual->bank->ifsc_code,
-//                 ];
-//         }
-//     endif;
-//         return response()->json([
-//             'status' => true,
-//             'message' => 'Virtual Funds have been successfully.',
-//             'data' => $result,
-//         ], 200);
-//     }
-// }
-
-
-// namespace App\Http\Controllers\Api;
-
-// use App\Http\Controllers\Controller;
-// use App\Models\Status;
-// use App\Models\VirtualRequest;
-// use Illuminate\Http\Request;
-// use Maatwebsite\Excel\Facades\Excel;
-// use App\Exports\VirtualRequestExport;
-
-// class VirtualRequestController extends Controller
 {
 
     public function virtualRequest()
