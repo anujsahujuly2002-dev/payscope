a8bb171034f4709a8943abab95961e080f29be3f
Anuj <anujsahujuly2002@gmail.com>
2024-10-13 12:13:18 +0530

    changes

--- app/Console/Commands/AutoTransactionUpdateWebhook.php
+++ app/Console/Commands/AutoTransactionUpdateWebhook.php
@@ -35,36 +35,40 @@ class AutoTransactionUpdateWebhook extends Command
             $query->from('auto_transaction_update_webhook_models')->selectRaw('COALESCE(MAX(transaction_id), 4188)');
         })->get();
         foreach ($getTransactions as $transaction):
-            $webhookUrl = ApiToken::where('user_id', $transaction->user_id)->latest()->first();
-            if ($webhookUrl->domain !== NULL) {
-                $parameters = [
-                    "status" => true,
-                    "msg" => "You're request has been complete",
-                    "data" => [
-                        "payout_ref" => $transaction->payout_ref,
-                        "utr_number" => $transaction->utr_number,
-                        "transaction_id" => $transaction->payout_id,
-                        "status" => ucfirst(strip_tags($transaction->status->name)),
-                        "account_number" => $transaction->account_number,
-                        "account_holder_name" => $transaction->account_holder_name,
-                        "ifsc_code" => $transaction->ifsc_code,
-                        "amount" => $transaction->amount,
-                        "charges" => $transaction->payoutTransactionHistories->charge,
-                    ],
-                ];
-                try{
-                    Http::retry(3, 100)->post($webhookUrl->domain ,$parameters);
-                    AutoTransactionUpdateWebhookModel::create([
-                        'user_id'=>$transaction->user_id,
-                        'transaction_id'=>$transaction->id,
-                        'webhook_url'=>$webhookUrl->domain 
-                    ]);
-                }catch(Exception $e) {
-                    Log::error($e->getMessage());
+            if(in_array($transaction->status_id,[2,3,4])):
+                $webhookUrl = ApiToken::where('user_id', $transaction->user_id)->latest()->first();
+                if ($webhookUrl->domain !== NULL) {
+                    $parameters = [
+                        "status" => true,
+                        "msg" => "You're request has been complete",
+                        "data" => [
+                            "payout_ref" => $transaction->payout_ref,
+                            "utr_number" => $transaction->utr_number,
+                            "transaction_id" => $transaction->payout_id,
+                            "status" => ucfirst(strip_tags($transaction->status->name)),
+                            "account_number" => $transaction->account_number,
+                            "account_holder_name" => $transaction->account_holder_name,
+                            "ifsc_code" => $transaction->ifsc_code,
+                            "amount" => $transaction->amount,
+                            "charges" => $transaction->payoutTransactionHistories->charge,
+                        ],
+                    ];
+                    try{
+                        Http::retry(3, 100)->post($webhookUrl->domain ,$parameters);
+                        AutoTransactionUpdateWebhookModel::create([
+                            'user_id'=>$transaction->user_id,
+                            'transaction_id'=>$transaction->id,
+                            'webhook_url'=>$webhookUrl->domain 
+                        ]);
+                    }catch(Exception $e) {
+                        Log::error($e->getMessage());
+                    }
+           
+                } else {
+                    Log::info("Webhook Url Not a registerd");
                 }
-            } else {
-                Log::info("Webhook Url Not a registerd");
-            }
+                    
+            endif;
 
 
         endforeach;
